1f98b538c3b718e704819efddf018195df7adc4b
Obinna Anyim <obinnanyim@gmail.com>
2020-12-06 05:02:10 +0100

    Component: Like Component

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/like';
 
 class Movies extends Component {
     state = {
@@ -10,6 +11,15 @@ class Movies extends Component {
         //console.log(movie);   
         const movies = this.state.movies.filter(m => m._id !== movie._id); 
         this.setState({ movies });    //Es6 simplified movies: movies
+    }
+    
+    handleLike = (movie) => {
+        //console.log('Like icon clicked....', movie);
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]}
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies });
     }
     
     render() {
@@ -28,6 +38,7 @@ class Movies extends Component {
                             <th>Stock</th>
                             <th>Rate</th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,13 +48,13 @@ class Movies extends Component {
                             <td>{ movie.genre.name }</td>
                             <td>{ movie.numberInStock }</td>
                             <td>{ movie.dailyRentalRate }</td>
+                            <td> <Like liked={ movie.liked } onClick={() => this.handleLike(movie) } /> </td>
                             <td><button onClick={ () => this.handleDelete(movie) } className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>
                     ))}                
                     </tbody>
                 </table>
-            </React.Fragment>
-            
+            </React.Fragment>            
         );
     }
 }
