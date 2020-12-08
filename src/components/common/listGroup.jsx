ee15a63be0273b2150cc7ab0d6685ab9fe5974df
Obinna Anyim <obinnanyim@gmail.com>
2020-12-08 14:44:00 +0100

    Filtering: Default Props

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const ListGroup = props => {
-    const { items, textProperty, valueProperty } = props;
+    const { items, textProperty, valueProperty, onItemSelect } = props;
     
     return <ul className="list-group">
-     { items.map(item => <li key={ item[valueProperty] } className="list-group-item">{ item[textProperty] }</li>)}        
+     { items.map(item => <li key={ item[valueProperty] } onClick={() => onItemSelect(item) } className="list-group-item">{ item[textProperty] }</li>)}        
     </ul>
 };
 
