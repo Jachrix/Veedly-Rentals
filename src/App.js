afbed64e80b6cec7700e5cd8dfa14bb6c53b6af1
Obinna Anyim <obinnanyim@gmail.com>
2020-12-14 10:57:42 +0100

    Routing: Adding the Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 //import logo from "./logo.svg";
-import "./App.css";
-import Movies from './components/movie';
+import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import Movies from './components/movie';
 import NotFound from './components/notFound';
 import Rentals from './components/rentals';
 import Customers from './components/customers';
+import NavBar from "./components/navBar";
+import "./App.css";
 
-function App() {
-  return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/notfound" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/notfound" />
-      </Switch>  
-    </main>
-  );
+class App extends Component {
+  render() { 
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/notfound" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/notfound" />
+          </Switch>  
+        </main>
+      </React.Fragment>
+    );
+  }
 }
-
+ 
 export default App;
