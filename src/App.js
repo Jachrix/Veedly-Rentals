4199863f75a807daed332160e2575ce89b384ed4
Obinna Anyim <obinnanyim@gmail.com>
2020-12-14 06:41:58 +0100

    Routing: Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 //import logo from "./logo.svg";
 import "./App.css";
 import Movies from './components/movie';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
 
 function App() {
   return (
     <main className="container">
-      <Movies/>
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/notfound" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/notfound" />
+      </Switch>  
     </main>
   );
 }
