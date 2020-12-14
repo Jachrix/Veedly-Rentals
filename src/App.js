337a84e5dea90c410aed738be5d7cc0ee9935a4e
Obinna Anyim <obinnanyim@gmail.com>
2020-12-14 11:43:22 +0100

    Routing: Linking the MovieForm

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NotFound from './components/notFound';
 import Rentals from './components/rentals';
 import Customers from './components/customers';
 import NavBar from "./components/navBar";
+import MovieForm from './components/movieForm';
 import "./App.css";
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm}/>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
