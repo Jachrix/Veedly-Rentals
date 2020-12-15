4acffa11c2d97b916f1ebba85237aec425fd66f8
Obinna Anyim <obinnanyim@gmail.com>
2020-12-15 06:39:10 +0100

    Forms: Controlled Element

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
-    username = React.createRef();
+    state = {
+        account: { username: '', password: '' }
+    };
+    //username = React.createRef();
     
     // componentDidMount(){
     //     this.username.current.focus();
@@ -10,11 +13,19 @@ class LoginForm extends Component {
     handleSubmit = e => {
         e.preventDefault();
         
-        const username = this.username.current.value;
+        //const username = this.username.current.value;
+        // ref ={this.username} as attribute in specified element
         
         // call to the server
-        console.log('form submitted ....', username);
+        console.log('form submitted ....');
     }
+    
+    handleChange = e => {
+        const account = {...this.state.account};
+        account.username = e.currentTarget.value;
+        this.setState({ account });
+    }
+    
     render() {
         return (
             <div>
@@ -22,7 +33,14 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit} >
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input ref={this.username} autoFocus id="username" type="text" className="form-control"/>
+                        <input 
+                            value={ this.state.account.username } 
+                            autoFocus 
+                            id="username" 
+                            type="text" 
+                            className="form-control"
+                            onChange={ this.handleChange }
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
