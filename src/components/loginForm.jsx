e8e9ecab0dfd389754a16f59241c1f4c8aae3165
Obinna Anyim <obinnanyim@gmail.com>
2020-12-20 06:41:01 +0100

    Form Validation: Disabling the Submit Button

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -113,7 +113,7 @@ class LoginForm extends Component {
                         error={ errors.password } 
                     />
                     
-                    <button className="btn btn-primary">Login</button>
+                    <button disabled={ this.validate() } className="btn btn-primary">Login</button>
                 </form>
             </div>
         );
