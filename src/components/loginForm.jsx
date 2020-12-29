5c7c440d7ba476be25f4e20b2c7ea284f478db06
Obinna Anyim <obinnanyim@gmail.com>
2020-12-29 09:49:20 +0100

    Forms: Extracting Reusable Form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Joi from 'joi-browser';
 import Input from './common/input';
+import Form from './common/form';
 
-class LoginForm extends Component {
+class LoginForm extends Form {
     state = {
-        account: { username: '', password: '' },
+        data: { username: '', password: '' },
         errors: { }
     };
     //username = React.createRef();
@@ -17,82 +18,14 @@ class LoginForm extends Component {
         username: Joi.string().required().label('Username'),
         password: Joi.string().required().label('Password')
     }
-    
-    
-    validate = () => {
-        const options = { abortEarly: false }
-        const { error } = Joi.validate(this.state.account, this.schema, options);
         
-        if(!error) return null;
-        
-        const errors = {};
-        
-        for (let item of error.details)
-            errors[item.path[0]] = item.message;
-            return errors;
-        
-        // console.log(result);
-        
-        // const errors = {};
-        
-        // const { account } = this.state;
-        
-        // if(account.username.trim() === '') 
-        //     errors.username = 'Username is required';
-        // if(account.password.trim() === '') 
-        //     errors.password = 'Password is required';
-            
-        // return Object.keys(errors).length === 0 ? null : errors;
-    }
-    
-    handleSubmit = e => {
-        e.preventDefault();
-        
-        //const username = this.username.current.value;
-        // ref ={this.username} as attribute in specified element
-        
-        const errors = this.validate();
-        this.setState({ errors: errors || {} });
-        if (errors) return;
-        
-        // call to the server
-        console.log('form submitted ....');
-    }
-    
-    validateProperty = ({ name, value }) => {
-        const obj = {[name]: value};
-        
-        const schema = {
-            [name]: this.schema[name]
-        }
-        
-        const { error } = Joi.validate(obj, schema);
-        
-        return error ? error.details[0].message : null;
-        
-        // if(name === 'username'){
-        //     if(value.trim() === '') return 'Username is required';
-        // }
-        // if(name === 'password'){
-        //     if(value.trim() === '') return 'Password is required';
-        // }
-    }
-    
-    handleChange = ({ currentTarget: input }) => {
-        const errors = {...this.state.errors }
-        const errorMessage = this.validateProperty(input);
-        
-        if(errorMessage) errors[input.name] = errorMessage;
-        else delete errors[input.name];
-        
-        const account = {...this.state.account};
-        account[input.name] = input.value;
-        
-        this.setState({ account, errors });
+    doSubmit = () => {
+         // call to the server
+         console.log('form submitted ....');
     }
     
     render() {
-        const { account, errors } = this.state;
+        const { data, errors } = this.state;
         
         return (
             <div>
@@ -100,14 +33,14 @@ class LoginForm extends Component {
                 <form onSubmit={ this.handleSubmit } >
                     <Input 
                         name="username" 
-                        value={ account.username } 
+                        value={ data.username } 
                         label="Username" 
                         onChange={ this.handleChange }
                         error={ errors.username } 
                     />
                     <Input 
                         name="password" 
-                        value={ account.password } 
+                        value={ data.password } 
                         label="Password" 
                         onChange={ this.handleChange }
                         error={ errors.password } 
