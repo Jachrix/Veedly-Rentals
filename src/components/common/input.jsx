d86ea165b06083937ef27f4879d63d24e433888c
Obinna Anyim <obinnanyim@gmail.com>
2020-12-29 10:34:20 +0100

    Forms: Extracting Helper Rendering Methods

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Input = ({name, label, value, error, onChange }) => {
+const Input = ({name, label, error, ...rest }) => {
     return (
         <div className="form-group">
             <label htmlFor={name}>{ label }</label>
             <input
+                {...rest}
                 id={name}
                 name={name}
-                value={ value } 
-                type="text" 
                 className="form-control"
-                onChange={ onChange }
             />
              {error && <div className="alert alert-danger">{ error }</div>}
         </div>);
